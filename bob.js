2934edc1f1369bab93c3add48ff83967e4bdaa63
Alexis246 <70736915+Alexis246@users.noreply.github.com>
2020-10-20 17:21:11 -0700

    Update bob.js

--- bob.js
+++ bob.js
@@ -1,9 +1,9 @@
 class Bob {
     constructor(x, y) {
       var options = {
-          'restitution':1.4,
+          'restitution':1,
           'friction':0,
-          'density':0.5,
+          'density':0.9,
           isStatic:false
       }
       this.body = Bodies.circle(x, y, 40, options);
